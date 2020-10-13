ab81242702a564c2838ebb2eb87234cfe5057eb6
Atieh Hamidi <hamidiatiye@gmail.com>
2020-10-13 19:12:13 +0200

    added some styling

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
-import React from "react";
-import logo from "./logo.svg";
+import React, { useState, useEffect } from "react";
+
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <div className="box">
+        <h3>Current Note</h3>
+        <button>Save Note</button>
+        <button>Start/Stop</button>
+      </div>
+      <div className="box">
+        <h3>Notes</h3>
+      </div>
+    </div>
+  );
 }
 
 export default App;
